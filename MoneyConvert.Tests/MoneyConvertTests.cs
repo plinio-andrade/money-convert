2ea53fbb8a399800dc5923a4259328b6366644ae
Plinio Andrade <andrade-plinio@outlook.com>
2024-04-02 10:51:05 -0300

    Finalizada primeira versão

---
+++ MoneyConvert.Tests/MoneyConvertTests.cs
@@ -0,0 +1,14 @@
+namespace MoneyConvert.Tests;
+
+[TestClass]
+public class MoneyConvertTests
+{
+    [TestMethod]
+    public void ShouldConvertDecimalToInt()
+    {
+        decimal valor = 279.98M;
+        var cents = valor.ToCents();
+
+        Assert.AreEqual(27998, cents);
+    }
+}
